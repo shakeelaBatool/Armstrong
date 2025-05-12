922b61832f4db51839451627a2f6b365754f1f59
shakeelaBatool <shakeelasadpara@gmail.com>
2025-05-12 11:08:46 -0700

    Add files via upload

---
+++ Armstrong.py
@@ -0,0 +1,13 @@
+number = int(input("Enter the number. "))
+str_number = str(number)
+num_digit = len(str_number)
+sum_power = 0
+tmp_num = number
+while tmp_num > 0 :
+      digit = tmp_num % 10
+      sum_power += digit ** num_digit
+      tmp_num //= 10
+if sum_power == number:
+    print(number, " is a Armstrong number")    
+else:
+    print(number, " is a not Armstrong number")
