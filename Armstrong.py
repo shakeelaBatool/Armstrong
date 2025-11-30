2873eb797019b531304e4e0f9216c57d70a10e30
Shakeela Batool <shakeelasadpara@gmail.com>
2025-11-30 14:48:24 +0500

    Fix indentation and formatting in Armstrong.py

--- Armstrong.py
+++ Armstrong.py
@@ -2,7 +2,7 @@ number = int(input(" Enter the number. "))
 str_number = str(number)
 num_digit = len(str_number)
 sum_power = 0
-tmp_num =number
+tmp_num = number
 while tmp_num > 0 :
       digit = tmp_num% 10
       sum_power += digit ** num_digit
@@ -11,6 +11,7 @@ if sum_power == number:
     print(f"{number} is an Armstrong number.")    
 else:
     print(f"{number} is a not Armstrong number.")
+
 
 
 
