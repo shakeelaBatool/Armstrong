017f4c1348ad05ef1291b90c9b585be86f0790cd
Shakeela Batool <shakeelasadpara@gmail.com>
2025-12-06 08:13:27 +0500

    Update Armstrong.py

--- Armstrong.py
+++ Armstrong.py
@@ -1,5 +1,5 @@
 number = int(input(" Enter the number. "))
-str_number = str(number)
+str_number =str(number)
 num_digit = len(str_number)
 sum_power = 0
 tmp_num = number
@@ -11,6 +11,7 @@ if sum_power ==number:
     print(f"{number} is an Armstrong number.")    
 else:
     print(f"{number} is a not Armstrong number.")
+
 
 
 
